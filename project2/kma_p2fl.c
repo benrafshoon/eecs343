048e1cffb1caf477c8219f1ebe676be1ea901f2e
Audrey Hosford <audreyhosford2014@u.northwestern.edu>
2013-10-29 23:54:31 -0500

    P2FL not working--says all pages already allocated, idk why

--- project2/kma_p2fl.c
+++ project2/kma_p2fl.c
@@ -49,58 +49,200 @@
  *  variables should be in all lower case. When initializing
  *  structures and arrays, line everything up in neat columns.
  */
-typedef struct {
+
+#define MINPOWER 32
+#define MAXBUFSIZE 8192
+#define FREELISTSIZE 10
+#define PAGES 9
+
+typedef struct Buf_{
     void* start;
-} buf_t;
+} Buf;
 
-typedef struct pagelist_struct {
+typedef struct PageList_ {
     kma_page_t* new_page;
-    struct pagelist_struct* next;
-} pagelist;
+    struct PageList_* next;
+} PageList;
 
-typedef struct {
+typedef struct FreeList_{
     int size;
-    pagelist* first;
     int used;
-    buf_t* free_buf_list;
-} freelist;
+    PageList* first;
+    Buf* free_buf_list;
+} FreeList;
 
-typedef struct {
-    freelist buf_array[10];
+typedef struct BufferList_{
+    FreeList bufArray[10];
     int used;
-} buffer_list;
+} BufferList;
 
 /************Global Variables*********************************************/
-kma_page_t* begin = NULL;
+kma_page_t* init = NULL;
 
 
 /************Function Prototypes******************************************/
+void SetupPage(kma_page_t* page);
+void InitBufArray(BufferList* list);
+void* GetBuffer(FreeList* from_list);
+void MakeBuffer(FreeList* from_list, void* ptr);
+void SlicePage(void* ptr, BufferList* list, int count);
+void AddPageToList(kma_page_t* page, FreeList* home_list);
+void FreePage(FreeList* list);
 
 /************External Declaration*****************************************/
 
 /**************Implementation***********************************************/
+void InitBufArray(BufferList* list){
+    int i;
+    int pow = 5;
+    for(i = 0; i < (FREELISTSIZE - 1); i++){
+        list->bufArray[i].size = 2 ^ pow;
+        pow++;
+        list->bufArray[i].first = NULL;
+    }
+    int j;
+    for (j = 0; j < FREELISTSIZE; j++){
+        list->bufArray[i].first = NULL;
+        list->bufArray[i].used = 0;
+        list->bufArray[i].free_buf_list = NULL;
+    }
+    list->bufArray[PAGES].size = sizeof(PageList) + sizeof(Buf);
+    list->used = 0;
+
+
+}
+
+void SlicePage(void* ptr, BufferList* list, int count){
+    Buf* header;
+    int i;
+    for (i = 0; i < count; i++){
+        header = (Buf*)(ptr + i * (sizeof(PageList) + sizeof(Buf)));
+        header->start = list->bufArray[PAGES].first;
+    }
+}
+
+void SetupPage(kma_page_t* page)
+{
+    //initialize free list
+    BufferList* buf_list = (BufferList*)page->ptr;
+    InitBufArray(buf_list);
+    int buf_count = (PAGESIZE - sizeof(buf_list)) / (sizeof(PageList) + sizeof(Buf));
+
+    void* begin = page->ptr + sizeof(buf_list);
+    SlicePage(begin, buf_list, buf_count);
+    AddPageToList(page, &buf_list->bufArray[PAGES]);
+
+}
+
+void MakeBuffer(FreeList* from_list, void* ptr){
+        int i;
+        for (i = 0; i < (PAGESIZE / from_list->size); i++){
+            void* newb = ptr + (i * from_list->size);
+            Buf* new_buf = (Buf*)newb;
+            new_buf->start = from_list->first;
+            from_list->free_buf_list = new_buf;
+        }
+}
+
+void AddPageToList(kma_page_t* page, FreeList* home_list){
+    BufferList* list = (BufferList*)init->ptr;
+    PageList* newPageList = (PageList*)GetBuffer(&list->bufArray[PAGES]);
+    newPageList->new_page = page;
+    newPageList->next = home_list->first;
+    home_list->first = newPageList;
+
+}
+
+void* GetBuffer(FreeList* from_list){
+
+    if (from_list->first == NULL){
+        kma_page_t* page = get_page();
+        void* p_start = page->ptr;
+        MakeBuffer(from_list, p_start);
+        AddPageToList(page, from_list);
+    }
+    Buf* newBuf = from_list->free_buf_list;
+    from_list->free_buf_list = (Buf*)newBuf->start;
+    newBuf->start = (void*)from_list;
+    from_list->used += 1;
+    BufferList* bufL = init->ptr;
+    void* rest_buf = (void*)newBuf + sizeof(Buf);
+    if (from_list == &bufL->bufArray[PAGES]){
+        return rest_buf;
+    } else {
+        bufL->used +=1;
+        return rest_buf;
+    }
+}
+
 
 void*
 kma_malloc(kma_size_t size)
 {
-    if (begin == NULL){
-        begin = get_page();
+    if (init == NULL){
+        init = get_page();
+        SetupPage(init);
     }
-    //start page or something
 
-    int adj_size = size + sizeof(buf_t);
-    buffer_list* list = (buffer_list*)begin->ptr;
-    freelist* req = NULL;
-    if (adj_size <= 32){
-        req = &list->buf_array[0];
+
+    int adj_size = size + sizeof(Buf);
+    BufferList* list = (BufferList*)init->ptr;
+    FreeList* req_mem = NULL;
+
+    int ndx = 0;
+    unsigned long long int bufsize = 1ULL << MINPOWER;
+    assert(adj_size <= MAXBUFSIZE);
+    while(bufsize < adj_size){
+        ndx++;
+        bufsize <<= 1;
     }
-  return NULL;
+    req_mem = &list->bufArray[ndx];
+    if (req_mem != NULL){
+        void* result_ptr = GetBuffer(req_mem);
+        return result_ptr;
+    }
+    else {
+        return NULL;
+    }
+
+}
+
+void FreePage(FreeList* list){
+    PageList* plist = list->first;
+    while (plist != NULL){
+        free_page(plist->new_page);
+        plist = plist->next;
+    }
 }
 
 void
 kma_free(void* ptr, kma_size_t size)
 {
-  ;
+    void* contents = (void*)ptr - sizeof(Buf);
+    Buf* bufContents = (Buf*)contents;
+    FreeList* list = (FreeList*)bufContents->start;
+    bufContents->start = list->free_buf_list;
+    list->free_buf_list = bufContents;
+    list->used -= 1;
+
+    if (list->used == 0) {
+        list->free_buf_list = NULL;
+        FreePage(list);
+        list->first = NULL;
+    }
+
+    BufferList* listFree = (BufferList*)init->ptr;
+    listFree->used -= 1;
+    if (listFree->used == 0){
+        PageList* pList = listFree->bufArray[PAGES].first;
+        while (pList->next != NULL){
+            free_page(pList->new_page);
+            pList = pList->next;
+        }
+        free_page(pList->new_page);
+        init = NULL;
+    }
 }
 
 #endif // KMA_P2FL
+
